667235eb2ae40b3cc0b51526042f666a320f7649
Isabella Teixeira <imt@bu.edu>
2025-04-07 15:26:54 -0400

    saving changes

--- app/r/[alias]/page.tsx
+++ app/r/[alias]/page.tsx
@@ -1,17 +1,20 @@
 import getCollection, { URLS_COLLECTION } from '@/db';
 import { redirect } from 'next/navigation';
 
-export default async function RedirectPage({
-                                               params,
-                                           }: {
-    params: { alias: string };
-}) {
+interface RedirectPageProps {
+    params: {
+        alias: string;
+    };
+}
+
+export default async function RedirectPage({ params }: RedirectPageProps) {
     const collection = await getCollection(URLS_COLLECTION);
     const result = await collection.findOne({ alias: params.alias });
 
     if (result) {
         redirect(result.url);
-    } else {
-        return <p className="p-4 text-red-500">Alias not found</p>;
     }
+
+    return <p className="p-4 text-red-500 text-center">Alias not found</p>;
 }
+
